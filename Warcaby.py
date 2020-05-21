1e05978ef96bf649c984dc282b5fd8a5aaed5b12
grzegorzpryjma99 <grzegorzpryjma@gmail.com>
2020-05-21 17:54:12 +0200

    Obsluga wyboru pionka

--- Warcaby.py
+++ Warcaby.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 
 # Definiuje
@@ -12,30 +13,32 @@ gold = (255, 215, 0)
 empty = 0
 rows = 8
 columns = 8
-przyjaciel = {'pionek': 1, 'krolowka': 3}
-wrog = {'pionek': 2, 'krolowka': 4}
+gracz1 = {'pionek': 1, 'krolowka': 3}
+gracz2 = {'pionek': 2, 'krolowka': 4}
+
+
 
 #tworze plansze
 def create_board():
     board = [[empty for column in range(columns)] for row in range(rows)]
     return board
 
-def place_starting_pieces():
+def poczatkowe_rozmieszczenie():
     #przydzielanie elementow i lokalizacji dla bialych
     for current_row in range(5, 8, 2):
         for current_column in range(0, 8, 2):
-            board[current_row][current_column] = przyjaciel['pionek']
+            board[current_row][current_column] = gracz1['pionek']
     for current_row in range(6, 7):
         for current_column in range(1, 8, 2):
-            board[current_row][current_column] = przyjaciel['pionek']
+            board[current_row][current_column] = gracz1['pionek']
 
     #przydzielanie lokalizacji planszy dla czarnych
     for current_row in range(0, 3, 2):
         for current_column in range(1, 8, 2):
-            board[current_row][current_column] = wrog['pionek']
+            board[current_row][current_column] = gracz2['pionek']
     for current_row in range(1, 2):
         for current_column in range(0, 8, 2):
-            board[current_row][current_column] = wrog['pionek']
+            board[current_row][current_column] = gracz2['pionek']
 
 
 def draw_board(board):
@@ -70,17 +73,44 @@ def draw_board(board):
                 pygame.draw.circle(screen, gold, rect_center, radius, granica)
             if board[row][column] == 4:
                 pygame.draw.circle(screen, gold, rect_center, radius, granica)
+    """
     rect2 = pygame.draw.rect(screen, green, [650, 50, 300, 100])
     rect2_center = rect2.center #       srodek prostokatu(na tekst o turze)
     text_surface_obj = font_obj.render('Hello World!', True, black)     #tu bedzie tekst z tura
     text_rect_obj = text_surface_obj.get_rect()
     text_rect_obj.center = rect2_center
     screen.blit(text_surface_obj, text_rect_obj)
+    """
+
+def tura(gracz):
+    font_obj = pygame.font.Font('freesansbold.ttf', 20)
+    rect2 = pygame.draw.rect(screen, green, [650, 50, 300, 100])
+    rect2_center = rect2.center  # srodek prostokatu(na tekst o turze)
+    if gracz == 1:
+        tekst = "Tura Gracza 1 "
+    if gracz == 2:
+        tekst = "Tura Gracza 2"
+    text_surface_obj = font_obj.render(tekst, True, black)     #tu bedzie tekst z tura
+    text_rect_obj = text_surface_obj.get_rect()
+    text_rect_obj.center = rect2_center
+    screen.blit(text_surface_obj, text_rect_obj)
+
+def wybor(board, gracz, x, y): #Gracz nie moze wybrac pustego pola lub nie swojego pionka
+    wybor_tab = board[y][x]
+    if wybor_tab == gracz1['pionek']: # TU BEDZIE JESZCZE OR i dla krolowki
+        return True
+    elif wybor_tab == gracz2['pionek']: #tu tez
+        print("To nie twoje")
+        return False
+    else:
+        print("Tu nawet nie ma pionka")
+        return False
 
 #Ustalam ze gra sie nie skonczyla
 game_over = False
+
 board = create_board()
-place_starting_pieces()
+poczatkowe_rozmieszczenie()
 
 # Inicjuje pygame
 pygame.init()
@@ -103,6 +133,11 @@ height = (window_height // total_rows)#75
 radius = (window_width // 20)#30
 granica = (window_width // 200)#3
 
+#Domyslnie bedzie zaczynal gracz 1 (bialy)
+gracz = 1
+print("Tura gracza: " , gracz)
+tura(1)
+
 #Gra
 while game_over == False:
     #print("cos")
@@ -121,6 +156,14 @@ while game_over == False:
             x = round(pozycja[0]//width,0) #zaokrąglam do 0 miejsa po przecinku
             y = round(pozycja[1]//height,0)
             print("kliknales ", (x,y)) #w kordach
+
+            #Tu sobie sprawdzam czy wybrany pionek jest gracza ktory ma ture w danym monencie
+            if wybor(board, gracz, x, y) == True:
+                pass
+            else:
+                continue
+
+            #jesli tak to ustalam nowa pozycja za pomoca przeciagniecia
             while True:
                 ###TO NIE DZIALA TAK JAKBYM CHCIAL!!!!!!!!!(DODALEM // ZAMIAST /, POWINNO BYC OK?)
                 event = pygame.event.wait()
@@ -133,6 +176,12 @@ while game_over == False:
                     nowy_y = round((nowa_pozycja[1] // height),0)
                     print("nowa pozycja ", (nowy_x, nowy_y))  # w kordach
 
+                    #print(gracz)
+
+
+
+
+
 
     # Limit 60fps
     clock.tick(60)
@@ -142,6 +191,8 @@ while game_over == False:
 
     # Update screen with what we drew
     pygame.display.flip()
+    #time.sleep(2)
+    #tura(2)
 
 # Exit the game
 pygame.quit()
